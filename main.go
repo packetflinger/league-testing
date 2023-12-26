759e0d8a95301a6381941ed727da70c01bba572d
Joe Reid <joe@joereid.com>
2023-12-26 10:07:27 -0500

    basic attempt at loading the textproto from disk and printing out stuff

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	lpb "github.com/packetflinger/league-testing/proto"
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+func main() {
+	leagues, err := parseLeague()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Leagues:")
+	for _, l := range leagues.GetLeague() {
+		fmt.Println("  ", l.GetName())
+	}
+}
+
+func parseLeague() (*lpb.Leagues, error) {
+	filename := "league1.textpb"
+	l := lpb.Leagues{}
+	contents, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	err = prototext.Unmarshal(contents, &l)
+	if err != nil {
+		return nil, err
+	}
+	return &l, nil
 }
